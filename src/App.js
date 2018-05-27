0ffa065e87cc1e31270cbe22c878232d9c087197
dennicza <dennicza@gmail.com>
2018-05-28 02:00:25 +0300

    fixed bug when there is no access_token in the URL

--- src/App.js
+++ src/App.js
@@ -133,6 +133,9 @@ class App extends Component {
   let parsed = queryString.parse(window.location.search);
   let accessToken = parsed.access_token;
 
+  if(!accessToken)
+    return;
+
   fetch('https://api.spotify.com/v1/me', {
     headers: {'Authorization': 'Bearer ' + accessToken}
   })
